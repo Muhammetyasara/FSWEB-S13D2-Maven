0a6071751762d149e5d9d352cda25c503d0ff243
MuhammetAkıtıcı <muhammetyasara@gmail.com>
2026-02-17 15:51:50 +0300

    Complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,79 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int number) {
+
+        String str = String.valueOf(Math.abs(number));
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static boolean isPerfectNumber(int number) {
+
+        if (number < 0) {
+            return false;
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+
+    public static String numberToWords(int number) {
+
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        String numStr = String.valueOf(number);
+        String result = "";
+
+        for (int i = 0; i < numStr.length(); i++) {
+            char digit = numStr.charAt(i);
+
+            switch (digit) {
+                case '0':
+                    result += "Zero ";
+                    break;
+                case '1':
+                    result += "One ";
+                    break;
+                case '2':
+                    result += "Two ";
+                    break;
+                case '3':
+                    result += "Three ";
+                    break;
+                case '4':
+                    result += "Four ";
+                    break;
+                case '5':
+                    result += "Five ";
+                    break;
+                case '6':
+                    result += "Six ";
+                    break;
+                case '7':
+                    result += "Seven ";
+                    break;
+                case '8':
+                    result += "Eight ";
+                    break;
+                case '9':
+                    result += "Nine ";
+                    break;
+            }
+        }
+
+        return result.trim();
+    }
+
+
 }
